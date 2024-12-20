6400c9cd678c85e40b407537f9988884ef71f062
Richard Rattmann <ems.rir@gmail.com>
2024-12-20 17:25:29 +0100

    advent of code 12 solved

--- advent/adv12.py
+++ advent/adv12.py
@@ -1,5 +1,4 @@
 import math
-from idlelib.pyshell import extended_linecache_checkcache
 
 from util.grid_util import grid_walk_val
 from util.timing import timed_run
@@ -46,17 +45,13 @@ def advent_12_step_2():
     added_grid = [ [False] * len(row) for row in grid ]
     dir_list = [IntVector2(*c) for c in [(1, 0), (0, 1), (-1, 0), (0, -1)]]
 
+    # recursively find the area
     def find_area(letter, pos: IntVector2, group):
         for next_pos in [ pos + dir for dir in dir_list ]:
             if next_pos.is_in_bounds(grid) and not next_pos.of_grid(added_grid) and next_pos.of_grid(grid) == letter:
                 group.append(next_pos)
                 next_pos.set_grid(added_grid, True)
                 find_area(letter, next_pos, group)
-
-    def check_line(dir, pos, group):
-        #while
-        pass
-
 
     result = 0
     for pos, val in grid_walk_val(grid):
@@ -68,24 +63,17 @@ def advent_12_step_2():
         find_area(val, pos, group)
         fence = []
         for p in group:
-            for fence_pos in [ p + dir for dir in dir_list ]:
+            for fence_pos, fdir in [ (p + dir, dir) for dir in dir_list ]:
                 if not fence_pos.is_in_bounds(grid) or fence_pos.of_grid(grid) != val:
-                    fence.append(fence_pos)
+                    #assign each fence position a perpendicular direction so identical positions can be differentiated
+                    check_dir = dir_list[(dir_list.index(fdir) + 1) % len(dir_list)]
+                    fence.append((fence_pos, check_dir))
 
-        fence_len = 0
-        ignore = [] # keep track of already used fence positions
-        for fpos in fence:
-            if fpos in ignore:
-                ignore.remove(fpos)
-                continue
-
-            if [ True for v in  [ (fpos + dir) for dir in fence ] if v in fence and v not in ignore ]:
-                ignore.append(fpos)
-            else:
-                fence_len += 1
-
+        # count all fences that don't have a neighbour in their direction
+        fence_len = len([ "" for f, fd in fence if (f + fd, fd) not in fence ])
         result += len(group) * fence_len
 
     print("result " + str(result))
+    #solves in 105 ms
 
-timed_run(advent_12_step_2())
+timed_run(advent_12_step_2)
